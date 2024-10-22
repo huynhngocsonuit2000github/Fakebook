51ec0947e2592e023516e724291d6d85948b29b3
Huynh Ngoc Son <huynhngocson@192.168.2.18>
2024-10-23 00:12:50 +0700

    FB001-14: Create Jenkins Job to Build the Images, Load Configuration, Build Manifest Deployment from Template, deploy to namespace

---
+++ Environments/Jenkins/UserService/config.groovy
@@ -0,0 +1,35 @@
+return [
+    // DockerHub repository
+    IMAGE_NAME: 'huynhngocsonuit2000docker/fakebook-userservice',
+    
+    // Docker & K8S configuration
+    DOCKERFILE: 'src/be/Services/Containerizations/UserService.Dockerfile',
+    SECRET_CONFIGMAP_PATH: 'Environments/Jenkins/UserService/Kubernetes/secrets-configmap/*.yaml',
+    SERVICES_PATH: 'Environments/Jenkins/UserService/Kubernetes/deployments-services/*.yaml',
+
+    // SQL Server Configuration
+    SQL_DEPLOYMENT_NAME: 'staging-user-deployment-sqlserver',
+    SQL_CONTAINER_NAME: 'mssql-server',
+    SQL_IMAGE: 'mcr.microsoft.com/mssql/server:2022-latest',
+    SQL_PORT: 1433,
+    SQL_REPLICAS: 1,
+    SQL_SECRET_NAME: 'staging-user-secret-credential',
+    SQL_SECRET_PASSWORD_KEY: 'SA_PASSWORD',
+    SQL_MEMORY_REQUEST: "2048Mi",
+    SQL_CPU_REQUEST: "500m",
+    SQL_MEMORY_LIMIT: "4096Mi",
+    SQL_CPU_LIMIT: "1",
+    SQL_SERVICE_NAME: 'staging-user-service-sqlserver',
+
+    // User Service Configuration
+    USER_DEPLOYMENT_NAME: 'staging-user-deployment-user-service',
+    USER_CONTAINER_NAME: 'userservice',
+    USER_PORT: 80,
+    USER_REPLICAS: 1,
+    USER_ENVIRONMENT: 'Development',
+    USER_DATABASE: 'UserDatabase',
+    USER_SECRET_NAME: 'staging-user-secret-credential',
+    USER_SECRET_PASSWORD_KEY: 'SA_PASSWORD',
+    USER_SERVICE_NAME: 'staging-user-service-user-service',
+    USER_NODE_PORT: 31000,
+]
---
+++ Environments/Jenkins/global-config.groovy
@@ -0,0 +1,6 @@
+return [
+    KUBECONFIG: '/root/kube-configs/admin.conf',
+    KUBE_CONTEXT: 'kubernetes-admin@kubernetes',
+    DOCKER_CREDENTIAL_ID: 'dockerhub-credentials',
+    NAMESPACE: 'staging-environment'
+]
