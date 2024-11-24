9ff8afe245a88a68f2f2e73d942247c78810fe27
Huynh Ngoc Son <huynhngocson@192.168.2.18>
2024-11-24 21:04:22 +0700

    FB001-23: Recreate Angular Typescript Project

--- Documents/MemoryDraft.groovy
+++ Documents/MemoryDraft.groovy
@@ -1,31 +1,3 @@
-import { combineReducers } from 'redux';
-import { accountReducer } from './account/reducers';
-import { configureStore } from '@reduxjs/toolkit'; 
-// Custom middleware for logging
-const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
-    return next(action);
-};
-
-const anotherMiddleware = (store: any) => (next: any) => (action: any) => {
-    // Custom logic here
-    return next(action);
-};
-
-// Combine reducers
-const rootReducer = combineReducers({
-    account: accountReducer,
-});
- 
-
-export type AppState = ReturnType<typeof rootReducer>;
-
-export default function configureAppStore() {
-    const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-
-    return configureStore({
-        reducer: rootReducer,
-        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loggerMiddleware, anotherMiddleware), // Add custom middleware
-        devTools: devTools ? devTools : undefined,
-    });
-}
+- Create new individual branch to manage version of all service image (Tag)
+Userservice: v0.0.2
+Accountservice: v0.0.5
