b5b00a9a768a53ee29f10771f89176f08618be10
Huynh Ngoc Son <huynhngocson@192.168.2.18>
2024-12-12 23:09:51 +0700

    FB001-49: Create CI/CD to deploy Ocelot to Cluster for two environments

---
+++ Environments/Jenkins/ApiGatewayService/production-config.groovy
@@ -0,0 +1,18 @@
+return [
+    // DockerHub repository
+    IMAGE_NAME: 'huynhngocsonuit2000docker/fakebook-apigateway',
+    
+    // Docker & K8S configuration
+    DOCKERFILE: 'src/be/Services/Containerizations/ApiGatewayService.Dockerfile',
+    SECRET_CONFIGMAP_PATH: 'Environments/Jenkins/ApiGatewayService/Kubernetes/secrets-configmap/*.yaml',
+    SERVICES_PATH: 'Environments/Jenkins/ApiGatewayService/Kubernetes/deployments-services/*.yaml',
+
+    // User Service Configuration
+    API_GATEWAY_ENVIRONMENT: 'Production',
+    API_GATEWAY_DEPLOYMENT_NAME: 'production-fakebook-apigateway-deployment-service',
+    API_GATEWAY_SERVICE_NAME: 'production-fakebook-apigateway-service-service',
+    API_GATEWAY_CONTAINER_NAME: 'apigatewayservice',
+    API_GATEWAY_PORT: 80,
+    API_GATEWAY_REPLICAS: 1,
+    API_GATEWAY_NODE_PORT: 32321,
+]
---
+++ Environments/Jenkins/ApiGatewayService/staging-config.groovy
@@ -0,0 +1,18 @@
+return [
+    // DockerHub repository
+    IMAGE_NAME: 'huynhngocsonuit2000docker/fakebook-apigateway',
+    
+    // Docker & K8S configuration
+    DOCKERFILE: 'src/be/Services/Containerizations/ApiGatewayService.Dockerfile',
+    SECRET_CONFIGMAP_PATH: 'Environments/Jenkins/ApiGatewayService/Kubernetes/secrets-configmap/*.yaml',
+    SERVICES_PATH: 'Environments/Jenkins/ApiGatewayService/Kubernetes/deployments-services/*.yaml',
+
+    // User Service Configuration
+    API_GATEWAY_ENVIRONMENT: 'Staging',
+    API_GATEWAY_DEPLOYMENT_NAME: 'staging-fakebook-apigateway-deployment-service',
+    API_GATEWAY_SERVICE_NAME: 'staging-fakebook-apigateway-service-service',
+    API_GATEWAY_CONTAINER_NAME: 'apigatewayservice',
+    API_GATEWAY_PORT: 80,
+    API_GATEWAY_REPLICAS: 1,
+    API_GATEWAY_NODE_PORT: 31321,
+]
